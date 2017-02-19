718d6a90a9fcdf2dd4eb89ee569c85a1ece21645
OctoNezd <nezd@protonmail.com>
2017-02-19 20:42:15 +0300

    typo

--- main.py
+++ main.py
@@ -749,7 +749,7 @@ class myHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 self.wfile.write(
-                    bytes(base % ("", messagehtml % ('danger', 'Bad request!'), version, str(timer_stop - timer_start)), 'utf-8')
+                    bytes(base % ("", messagehtml % ('danger', 'Bad request!'), version, str(timer_stop - timer_start)), 'utf-8'))
         except Exception as e:
             self.send_response(500)
             self.send_header('Content-type', 'text/html')

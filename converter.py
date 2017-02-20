d10f1c20029eef0f0e992a506cd8eb9f4a2fd7dd
Martin Montane <martin_montane_6@hotmail.com>
2017-02-19 21:08:54 -0300

    converter finished

--- converter.py
+++ converter.py
@@ -1,5 +1,7 @@
 import dataset
-import pickles
+import pickle
+from uuid import uuid4
+import json
 
 with open('plugins.pickle', 'rb') as f:
     plugins = pickle.load(f)
@@ -7,22 +9,33 @@ with open('plugins.pickle', 'rb') as f:
 with open('users.pickle', 'rb') as f:
     users = pickle.load(f)
 
-{"1": {"TitleID": "0004000000198E00", 
-       "name": "Animal Crossing: New Leaf Cheat-Menu", 
-       "developer": "martor", 
-       "devsite": "http://example.com/", 
-       "desc": "asdf", 
-       "plg": "http://example.com/example.plg", 
-       "added": "2017-02-19 16:13", 
-       "timestamp": 1487531598.60032, 
-       "version": "3", 
-       "compatible": "universal", 
-       "pic": "http://rydog199.github.io/images/animal.png", 
-       "approved": false}}
-
-"martinmontane6@gmail.com": ["206c57add2a200e7ecd5ae2c184ea60d7a5bbc00e4d9b5560f4b00f290cbb6f8c2f7a6fdea735afd20462a7da4aac96882cc163409c2ef2f46262a8256406ce9", true, [1]]
-
 db=dataset.connect("sqlite:///plugs.db")
-usersdb=db.get_table("Users", primary_id="uuid", pirmary_type="String(36)")
+usersdb=db.get_table("Users", primary_id="uuid", primary_type="String(36)")
+pluginsdb = db["Plugins"]
+for i in dict(users).keys():
+    email=i
+    uuid=str(uuid4())
+    phash=users[i][0]
+    activate=users[i][1]
+    plugins=json.dumps(users[i][2])
+    newUser={'uuid': uuid, 'email': email, 'hash': phash, 'plugins': plugins, "activate": activate}
+    if usersdb.find_one(email=email)==None:
+      usersdb.insert(newUser)
 
-for i in users.keys()
+with open('plugins.pickle', 'rb') as f:
+    plugins = pickle.load(f)
+
+def lookForUploader(pid):
+  for i in usersdb.all():
+    i["plugins"]=json.loads(i["plugins"])
+    if pid in i["plugins"]:
+      return i["email"]
+
+apidata = {}
+copy = dict(plugins)
+for item in copy.keys():
+  if item!=0:
+    if pluginsdb.find_one(id=item)==None:
+      plugin = copy[item]
+      plugin['uploader'] = lookForUploader(item)
+      pluginsdb.insert(plugin)

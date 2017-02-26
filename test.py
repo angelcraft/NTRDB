aac69859ccfff22b200417c619022b12b88fe756
OctoNezd <nezd@protonmail.com>
2017-02-26 08:50:34 +0300

    Improved error handling in tests

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen, build_opener
 from urllib.parse import urlencode
+from urllib.error import HTTPError
 from subprocess import Popen, PIPE, DEVNULL, TimeoutExpired
 from time import sleep
 from os import remove
@@ -27,10 +28,14 @@ server = Popen(['python3', 'main.py', '--tests', 'True'],
 print("Sleeping 20 seconds to wait init")
 sleep(20)
 print("Checking index page...")
-if 'WELCOME TO NTR PLUGIN DATABASE' in str(urlopen('http://127.0.0.1:8080/index').read(), 'utf-8').upper():
-    print("Index:OK")
-else:
-    print("Index:FAIL")
+try:
+    if 'WELCOME TO NTR PLUGIN DATABASE' in str(urlopen('http://127.0.0.1:8080/index').read(), 'utf-8').upper():
+        print("Index:OK")
+    else:
+        print("Index:FAIL")
+        errcounter = errcounter + 1
+except HTTPError:
+    print("Index:FAIL(server returned non-200)")
     errcounter = errcounter + 1
 print("Logging in as admin...")
 url = 'http://127.0.0.1:8080/login'
@@ -38,15 +43,19 @@ data = urlencode({'rtype': 'loginpg',
                   'email': 'test@test.test',
                   'pword': 'test'
                   }).encode('utf-8')
-content = urlopen(url=url, data=data)
-cookies = parseCookie(content.getheader('Set-Cookie'))
-if 'AToken' in cookies:
-    print("Login:OK")
-    cookie = cookies['AToken']
+try:
+    content = urlopen(url=url, data=data)
+except HTTPError:
+    print("Login:FAIL")
 else:
-    print("Havent received cookie!")
-    print("Index:FAIL")
-    errcounter = errcounter + 1
+    cookies = parseCookie(content.getheader('Set-Cookie'))
+    if 'AToken' in cookies:
+        print("Login:OK")
+        cookie = cookies['AToken']
+    else:
+        print("Havent received cookie!")
+        print("Login:FAIL")
+        errcounter = errcounter + 1
 server.kill()
 if errcounter == 0:
     print("Tests were completed succesfully.")
